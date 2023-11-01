6709d5a279e9c1de528e8edfcc21a0ddc31c5e07
GHes31415 <sebasguthdz@gmail.com>
2023-10-31 23:23:45 -0400

    several models working

---
+++ example.py
@@ -0,0 +1,18 @@
+import math
+import os
+import time
+
+import matplotlib.pyplot as plt
+import numpy as np
+import ot as pot
+import torch
+import torchdyn
+from torchdyn.core import NeuralODE
+from torchdyn.datasets import generate_moons
+
+from torchcfm.conditional_flow_matching import *
+from torchcfm.models.models import *
+from torchcfm.utils import *
+
+# savedir = "models/8gaussian-moons"
+# os.makedirs(savedir, exist_ok=True)
